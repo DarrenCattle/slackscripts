160058ee5a02256d46f33df916138e850900afa0
Darren Cattle <darren@darrencattle.com>
2015-05-18 03:39:15 -0500

    working code
    no more huge loops, should be low memory usage but not 100% optimal,
    checks every second so hopefully beating out human users at this point

--- slackspam.js
+++ slackspam.js
@@ -16,6 +16,7 @@ var jq = document.createElement('script');
 jq.src = "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
 var a = 0;
 var msgbox = $('#message-input')[0];
+var done = false;
 
 function kush() {
 	var d = new Date();
@@ -23,23 +24,28 @@ function kush() {
 	var minute = d.getMinutes();
 	var seconds = d.getSeconds();
 	var hh = 3;
-	var mm = 25;
+	var mm = 34;
 	var ss = 60;
-	var four = minute==mm && (hour==hh || hour==16) && seconds <= ss;
+	var four = minute==mm && (hour==hh || hour==16) && seconds <= ss && !done;
 	console.log(hour, minute, seconds, four);
 	if(four)
 	{
+		done = true;
 		kushcommands();
-		a=0;
-		setTimeout(kush,10000);
+		setTimeout(kush,40000000); //4,310,000,000 would be more ideal
 	}
-	setTimeout(kush,10000);
+	else
+	{
+		setTimeout(kush,1000);
+	}
 }
 
 function kushcommands() {
 	var commands = ["pay me", "ridbot echo pay me", "ridbot echo molly pay d 1"];
-	if(a>commands.length)
+	if(a>=commands.length) {
+		a=0;
 		console.log("done");
+	}
 	else {
 		console.log("else: " + a);
 		msgbox.value = commands[a];
@@ -49,3 +55,24 @@ function kushcommands() {
 	}
 }
 kush();
+
+var jq = document.createElement('script');
+jq.src = "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
+var a = 0;
+var msgbox = $('#message-input')[0];
+
+function kushcommands() {
+	var commands = ["pay me", "ridbot echo pay me", "ridbot echo molly pay d 1"];
+	if(a>=commands.length) {
+		a=0;
+		console.log("done");
+	}
+	else {
+		console.log("else: " + a);
+		msgbox.value = commands[a];
+		$('#message-form').submit();
+		a++;
+		setTimeout(kushcommands,1000);
+	}
+}
+kushcommands();
